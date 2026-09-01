06673edb2db95712c99a0b0e6d481c20c8584612
Zeenath Ziyan <Zeenathziyan042@gmail.com@gmail.com>
2026-09-01 19:59:38 +0530

    Add alert analytics

--- analytics/analytics.js
+++ analytics/analytics.js
@@ -2,10 +2,12 @@ function getAlertSummary(alerts) {
     return {
         total: alerts.length,
         newAlerts: alerts.filter(a => a.status === "NEW").length,
-        highSeverity: alerts.filter(a => a.severity === "HIGH").length
+        highSeverity: alerts.filter(a => a.severity === "HIGH").length,
+        mediumSeverity: alerts.filter(a => a.severity === "MEDIUM").length,
+        lowSeverity: alerts.filter(a => a.severity === "LOW").length
     };
 }
 
-module.exports = {
+module.exports={
     getAlertSummary
 };
